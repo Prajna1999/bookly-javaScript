b30d3c8dcdffd5f6c832e6afd3e4e448ec56f0e4
Prajna1999 <gituprajna20@gmail.com>
2022-05-17 17:12:20 +0530

    refactor and book class

--- src/book.js
+++ src/book.js
@@ -1,5 +1,56 @@
 class Book{
-    constructor(book){
+
+    #ref="";
+    #publishedDate;
+    static count=0;
+    static getBookCount(){
+        return Book.count;
+    }
+
+
+    constructor(volume){
         // define properties
+     
+
+        this.title=volume.volumeInfo.title,
+        this.authors=volume.volumeInfo.authors[0],
+        this.#publishedDate=new Date( volume.volumeInfo.publishedDate),
+        this.thumbnail=volume.volumeInfo.imageLinks.thumbnail,
+        this.previewLink=volume.volumeInfo.previewLink,
+        this.description=volume.volumeInfo.description
+        Book.count++;
+        this.#ref="praj"+(Math.random()*10).toString(16).substring(2,12);
+    }
+    getYear(){
+        return this.#publishedDate.getFullYear();
+    }
+    
+    get id(){
+        return this.#ref.toLowerCase();
+    }
+    set id(val){
+        console.log('This cannot be changed', val);
+        return false;
     }
 }
+
+class Store{
+    static getBooks(){
+        let books;
+        if(!localStorage.getItem("books")){
+            books=[]
+        }else{
+            books=JSON.parse(localStorage.getItem("books"));
+        }
+        return books;
+    }
+
+    static addBook(book){
+        const books=this.getBooks();
+        books.push(book);
+        localStorage.setItem("books", JSON.stringify(books));
+    }
+
+}
+
+export  {Book, Store};
--- src/script.js
+++ src/script.js
@@ -1,24 +1,80 @@
-const main=document.querySelector("main");
+import {Book} from "./book.js";
+import { Store } from "./book.js";
 const searchInput=document.querySelector(".searchBox-form input[type=text]");
 const form=document.querySelector(".searchBox-form");
+const modal=document.querySelector(".modal");
 let books=[];
 
+function showPopup(message){
+    modal.innerText=message;
+    modal.classList.add("show");
+    setTimeout(()=>{
+        modal.classList.remove("show");
+    },2000);
+   
+}
 
+function buildBooks(){
+    const grid=document.querySelector(".grid");
+    grid.insertAdjacentHTML("beforebegin", `${Book.getBookCount()} books are found.`)
+    grid.innerHTML=books.map((b)=>{
+        return `<div class="books" data-ref="${b.id}">
+        
+        <a href="${b.previewLink}">
+        <img src="${b.thumbnail}">
+        </a>
+        <h2>${b.title} (${b.getYear()})</h2>
+        <p>Author(s): ${b.authors}</p>
+        <p>Descirption: ${b.description}</p>
+
+        </div>
+        `
+    }).join("")
+
+}
 
 function fetchAPI(queryString){
     const options={
         method:'GET',
-        q:queryString
+        // q:queryString
     }
 
     const url=`https://www.googleapis.com/books/v1/volumes?q=${queryString}`
     const req=new Request(url,options);
     
     fetch(req)
-    .then((response)=>response.json())
-    .then((result)=>console.log(result));
+        .then((response)=>{
+            if(!response.ok){
+                throw new Error(response.statusText);
+            }
+
+            return response.json()
+        }).then(result=>{
+            // do something
+            if(result.items===undefined){
+                // throw new Error('No such book exist');
+                showPopup("No results found!");
+                
+            }
+            books=result.items.map((volume)=>{
+                
+                const book= new Book(volume);
+                Store.addBook(book);
+                return book;
+                
+            });
+            console.log(books);
+            // localStorage.setItem("books",JSON.stringify(books));
+           
+            buildBooks();
+            
+
+        }).catch((e)=>{
+            console.log(e.message);
+        })
 
 }
+
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -26,13 +82,8 @@ form.addEventListener("submit", (e)=>{
     fetchAPI(searchInput.value);
 });
 
+// the below is a hack. To be added to local storage later
+document.addEventListener("DOMContentLoaded", fetchAPI(searchInput.value));
 
-
-// searchInput.addEventListener("keyup", (e)=>{
-//     if(e.key==="Enter" && e.target.value){
-//         // fetchAPI(e.target.value);
-//         console.log(e.target.value)
-//     }
-// })
 
 
